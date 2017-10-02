e31d9f6cec11b1c8d76310f0aaba4e16a00fb9db
dermio <thisdjay@gmail.com>
2017-10-02 09:41:54 -0700

    Added delete route.

--- server/server.js
+++ server/server.js
@@ -45,5 +45,19 @@ app.put('/lions/:id', function(req, res) {
   }
 });
 
+// Added delete route from Scott example
+app.delete('lions/:id', function (req, res) {
+  var lion = _.findIndex(lions, {id: req.params.id});
+
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(lion, 1);
+    res.send(deletedLion);
+  }
+});
+
+
 app.listen(3000);
 console.log('on port 3000');
