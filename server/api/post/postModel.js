c1e317fa4596ed53b74c9b7979a42da1e39de3bf
dermio <thisdjay@gmail.com>
2017-10-07 10:10:03 -0700

    Edited and made comments to postModel.js.

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -12,10 +12,16 @@ var PostSchema = new Schema({
     type: String,
     required: true
   },
-  // array of ids from the users
-  author: [{type: Schema.Types.ObjectId, ref: 'user'}],
+  // array of ids from the users, categories
+  author: {
+    type: Schema.Types.ObjectId, // referencing line 2 Schema
+    ref: 'user', // references the user collection in userModel.js, line 12
+    required: true // added this, should be required
+  },
 
-  categories: [{type: Schema.Types.ObjectId, ref: 'category'}]
+  categories: [
+    {type: Schema.Types.ObjectId, ref: 'category'}
+  ]
 });
 
 module.exports = mongoose.model('post', PostSchema);
